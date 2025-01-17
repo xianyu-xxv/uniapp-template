e60a0f37763d4c889170cefab3ceaf1bd86581bd
leiliy@qq.com <leiliy@qq.com>
2025-01-17 10:14:05 +0800

    工具类添加一个快捷路由方法

--- common/tools.js
+++ common/tools.js
@@ -1,10 +1,20 @@
+import {
+	router,
+	RouterMount
+} from '@/common/router.js';
 const install = (Vue, vm) => {
 	
 	Vue.prototype.$t = {
 		// 测试加法
 		toUpperCase(arg){
 			return arg && arg.toUpperCase();
-		}
+		},
+		go(path, params) {
+			router.push({
+				name: path,
+				params: params
+			});
+		},
 	}
 }
 
